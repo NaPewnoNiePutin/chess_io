0bac098c6fedd3f52066f80a6fcaa990bad23474
NaPewnoNiePutin <napewnonieputin@gmail.com>
2021-05-17 11:08:38 +0200

    poprakwi

--- sites/scripts.js
+++ sites/scripts.js
@@ -4,20 +4,41 @@ function fprofil1(){
 
 }
 
+function cofaj1(){
+    document.getElementById("i1").innerHTML = "♖"
+    document.getElementById("i1").style.fontSize = "60px";
+}
+
 function fprofil2(){
     document.getElementById("i2").innerHTML = "Rozegrane partie: 632"
     document.getElementById("i2").style.fontSize = "20px";
 
 }
 
+function cofaj2(){
+    document.getElementById("i2").innerHTML = "♖"
+    document.getElementById("i2").style.fontSize = "60px";
+}
+
 function fprofil3(){
-    document.getElementById("i3").innerHTML = "Z chessio od 16-05-2021"
+    document.getElementById("i3").innerHTML = "Ranking: 1678"
     document.getElementById("i3").style.fontSize = "20px";
 
 }
 
+function cofaj3(){
+    document.getElementById("i3").innerHTML = "♖"
+    document.getElementById("i3").style.fontSize = "60px";
+}
+
 function fprofil4(){
-    document.getElementById("i4").innerHTML = "Z chessio od 16-05-2021"
+    document.getElementById("i4").innerHTML = "Czas gry: 61 godzin"
     document.getElementById("i4").style.fontSize = "20px";
 
 }
+
+function cofaj4(){
+    document.getElementById("i4").innerHTML = "♖"
+    document.getElementById("i4").style.fontSize = "60px";
+}
+
