40faa9e057fe7cea31c8a1d0410e493eaa34c6e1
NaPewnoNiePutin <napewnonieputin@gmail.com>
2021-05-17 10:56:14 +0200

    poprawki

---
+++ sites/scripts.js
@@ -0,0 +1,23 @@
+function fprofil1(){
+    document.getElementById("i1").innerHTML = "Z chessio od 16-05-2021"
+    document.getElementById("i1").style.fontSize = "20px";
+
+}
+
+function fprofil2(){
+    document.getElementById("i2").innerHTML = "Rozegrane partie: 632"
+    document.getElementById("i2").style.fontSize = "20px";
+
+}
+
+function fprofil3(){
+    document.getElementById("i3").innerHTML = "Z chessio od 16-05-2021"
+    document.getElementById("i3").style.fontSize = "20px";
+
+}
+
+function fprofil4(){
+    document.getElementById("i4").innerHTML = "Z chessio od 16-05-2021"
+    document.getElementById("i4").style.fontSize = "20px";
+
+}
